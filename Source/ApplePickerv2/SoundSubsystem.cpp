72c93b61881723d243d1bd5820c4b61353d58202
IceStorman <vladgamedeveloper@gmail.com>
2024-02-16 20:46:06 +0200

    Added sound subsystem

--- Source/ApplePickerv2/SoundSubsystem.cpp
+++ Source/ApplePickerv2/SoundSubsystem.cpp
@@ -1,5 +1,16 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "SoundSubsystem.h"
 
+void USoundSubsystem::SetSoundMap(TMap<FName, USoundCue*> newMap)
+{
+	soundMap = newMap;
+}
+
+void USoundSubsystem::PlaySound(FName name)
+{
+	if (!soundMap[name])
+	{
+		GEngine->AddOnScreenDebugMessage(1, 5, FColor::Cyan, "Can't find a sound!");
+		return;
+	}
+	UGameplayStatics::PlaySound2D(GetWorld(), soundMap[name]);
+}
--- Source/ApplePickerv2/SoundSubsystem.h
+++ Source/ApplePickerv2/SoundSubsystem.h
@@ -1,17 +1,22 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Subsystems/Subsystem.h"
+#include "Sound/SoundCue.h"
+#include "Kismet/GameplayStatics.h"
 #include "SoundSubsystem.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class APPLEPICKERV2_API USoundSubsystem : public UWorldSubsystem
 {
 	GENERATED_BODY()
-	
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Sound")
+	void SetSoundMap(TMap<FName, USoundCue*> newTMap);
+	UFUNCTION(BlueprintCallable, Category = "Sound")
+	void PlaySound(FName name);
+
+private:
+	TMap<FName, USoundCue*> soundMap;
 };
